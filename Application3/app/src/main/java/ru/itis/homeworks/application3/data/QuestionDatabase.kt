16bdb950ea2152c60660cd6bbad8a04ae0466826
crewcrew3 <143967753+crewcrew3@users.noreply.github.com>
2024-11-28 22:31:12 +0300

    Database has been updated

--- Application3/app/src/main/java/ru/itis/homeworks/application3/data/QuestionDatabase.kt
+++ Application3/app/src/main/java/ru/itis/homeworks/application3/data/QuestionDatabase.kt
@@ -8,46 +8,58 @@ object QuestionDatabase {
     val questionList: List<Question> = listOf(
         Question (
             id = 0,
-            text = "Сколько будет 1+1?",
+            text = "Самая важная дисциплина в ИТИСе?",
             answers = listOf(
                 Answer (
+                    id = 0,
+                    text = "Русский язык",
+                ),
+                Answer (
                     id = 1,
-                    text = "1",
+                    text = "История"
                 ),
                 Answer (
                     id = 2,
-                    text = "2"
+                    text = "Физкультура"
                 ),
                 Answer (
                     id = 3,
-                    text = "3"
+                    text = "Алгебра и геометрия"
                 ),
                 Answer (
                     id = 4,
-                    text = "Я не знаю"
+                    text = "ОРИС"
                 ),
                 Answer (
                     id = 5,
-                    text = "Да"
+                    text = "Право"
                 ),
+                Answer (
+                    id = 6,
+                    text = "Основы российской государственности"
+                )
             )
         ),
 
         Question (
             id = 1,
-            text = "Любимый принцип из SOLID Абрамского?",
+            text = "Любимый принцип SOLID М.М.Абрамского?",
             answers = listOf(
                 Answer (
-                    id = 1,
-                    text = "Open-closed principle"
+                    id = 0,
+                    text = "Принцип Барбары Лисков"
                 ),
                 Answer (
-                    id = 2,
+                    id = 1,
                     text = "Liskov substitution principle"
                 ),
                 Answer (
-                    id = 3,
+                    id = 2,
                     text = "Interface segregation principle"
+                ),
+                Answer (
+                    id = 3,
+                    text = "Принцип подстановки Барбары Лисков"
                 ),
                 Answer (
                     id = 4,
@@ -55,21 +67,21 @@ object QuestionDatabase {
                 ),
                 Answer (
                     id = 5,
-                    text = "Dependency inversion principle"
+                    text = "LSP"
                 )
             )
         ),
 
         Question (
             id = 2,
-            text = "Как зовут преподавательницу, которая ведет практику по матстату?",
+            text = "Как зовут преподавательницу, ведущую практику по матстату?",
             answers = listOf(
                 Answer (
-                    id = 1,
+                    id = 0,
                     text = "София Альфисовна"
                 ),
                 Answer (
-                    id = 2,
+                    id = 1,
                     text = "Софья Альфисовна"
                 )
             )
@@ -80,50 +92,131 @@ object QuestionDatabase {
             text = "Можно ли продлить дедлайн?",
             answers = listOf(
                 Answer (
-                    id = 1,
+                    id = 0,
                     text = "Нет"
                 ),
                 Answer (
-                    id = 2,
+                    id = 1,
                     text = "Конечно! (ложь)"
-                ),
-                Answer (
-                    id = 3,
-                    text = "Земля вам пухом"
                 )
             )
         ),
 
         Question (
             id = 4,
-            text = "Я не знаю какой вопрос задать",
+            text = "Кто самый крутой специалист в математической логике?",
             answers = listOf(
                 Answer (
+                    id = 0,
+                    text = "Алан Тьюринг"
+                ),
+                Answer (
                     id = 1,
-                    text = "Да"
+                    text = "Джон фон Нейман"
                 ),
                 Answer (
                     id = 2,
-                    text = "Нет"
-                )
+                    text = "Бертран Рассел"
+                ),
+                Answer (
+                    id = 3,
+                    text = "Марат Мирзаевич Арсланов"
+                ),
+                Answer (
+                    id = 4,
+                    text = "Эдгар Кодд"
+                ),
             )
         ),
 
         Question (
             id = 5,
-            text = "Кошки или собаки?",
+            text = "За какое время можно успеть подготовиться к экзамену?",
             answers = listOf(
                 Answer (
+                    id = 0,
+                    text = "Два месяца"
+                ),
+                Answer (
                     id = 1,
-                    text = "Кошки"
+                    text = "Месяц"
                 ),
                 Answer (
                     id = 2,
-                    text = "Собаки"
+                    text = "Две недели"
                 ),
                 Answer (
                     id = 3,
-                    text = "Всех люблю"
+                    text = "Одна недели"
+                ),
+                Answer (
+                    id = 4,
+                    text = "Одна ночь"
+                ),
+                Answer (
+                    id = 5,
+                    text = "Что такое готовиться к экзамену?"
+                )
+            )
+        ),
+
+        Question (
+            id = 6,
+            text = "Котики или собачки?",
+            answers = listOf(
+                Answer (
+                    id = 0,
+                    text = "Коты"
+                ),
+                Answer (
+                    id = 1,
+                    text = "Собаки"
+                ),
+                Answer (
+                    id = 2,
+                    text = "Всех люблю!"
+                ),
+                Answer (
+                    id = 3,
+                    text = "Попугаи"
+                )
+            )
+        ),
+
+        Question (
+            id = 7,
+            text = "Какова вероятность уехать на лифте в двойке с первого этажа если вы идете к третьей паре?",
+            answers = listOf(
+                Answer (
+                    id = 0,
+                    text = "0%"
+                ),
+                Answer (
+                    id = 1,
+                    text = "100%"
+                ),
+                Answer (
+                    id = 2,
+                    text = "50/50"
+                )
+            )
+        ),
+
+        Question (
+            id = 8,
+            text = "Нужна ли математика программисту?",
+            answers = listOf(
+                Answer (
+                    id = 0,
+                    text = "Да, в любом случае"
+                ),
+                Answer (
+                    id = 1,
+                    text = "Нет"
+                ),
+                Answer (
+                    id = 2,
+                    text = "Зависит от направления"
                 )
             )
         )
